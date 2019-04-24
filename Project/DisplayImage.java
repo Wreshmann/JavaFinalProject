55e912f4865d12665b82762a144c945b18e5b3a5
Stephen Baker <stephenbaker@Stephens-MacBook-Pro-2.local>
2019-04-24 17:19:22 -0400

    Second commit

--- Project/DisplayImage.java
+++ Project/DisplayImage.java
@@ -1,6 +1,7 @@
 package Project;
 
 import java.awt.Canvas;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -12,18 +13,16 @@ import javax.imageio.ImageIO;
 public class DisplayImage extends Canvas {
 	private BufferedImage img;
 	private BufferedImage defaultImage;
-	private final int DEFAULT = 0;
 	private int[] planetCors; //tbd.. may not actually use it
-	private int w;
-	private int h;
+	private boolean drawBackground;
+	private Dimension size;
+	private int buffer;
 
 	
 	DisplayImage(){
-		File f = new File("Solar_System.jpg"); //put default image file here
+		File f = new File("background.png"); 
 		try {
 			defaultImage = ImageIO.read(f);
-//			w = defaultImage.getWidth();
-//			h = defaultImage.getHeight();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -33,27 +32,48 @@ public class DisplayImage extends Canvas {
 	
 	public void setImage(BufferedImage i) {
 		img = i;
-//		w = i.getWidth();
-//		h = i.getHeight();
 	}
 	
 	public void paint(Graphics gp) {
 		Graphics2D g = (Graphics2D) gp;
-		g.drawImage(img,null,0,0);
-	}
-	
-	public void paint(Graphics gp, int zero) {
-		Graphics2D g = (Graphics2D) gp;
-		if(zero == 0 && defaultImage != null) {
+		if(drawBackground) {
 			g.drawImage(defaultImage,null,0,0);
-//			w = defaultImage.getWidth();
-//			h = defaultImage.getHeight();
+			size = new Dimension(
+					defaultImage.getWidth()+buffer, defaultImage.getHeight());
+		}
+		else {
+			if(img != null) {
+				g.drawImage(img,null,0,0);
+				size = new Dimension(img.getWidth()+buffer, img.getHeight());
+			}
 		}
 	}
+	
 	
 	public Object getImageInfo() {
 		return null; //should return any relative info about it
 	}
 	
+	
+	public void drawBackground(Graphics g) {
+		drawBackground = true;
+		paint(g);
+	}
+	
+	
+	public void showImage(Graphics g) {
+		drawBackground = false;
+		paint(g);
+	}
+	
+	
+	public Dimension getSize() {
+		return size;
+		
+	}
+	
+	public void addBuffer(int b) {
+		buffer = b;
+	}
 	
 }
--- Project/SolarSystem.java
+++ Project/SolarSystem.java
@@ -4,30 +4,50 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.PathIterator;
+import java.awt.image.BufferedImage;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class SolarSystem extends JFrame implements ActionListener {
+public class SolarSystem extends JFrame implements ActionListener, MouseListener {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private DisplayImage display;
-	private JButton[] buttons = new JButton[5];
+	private JButton[] buttons;
+	private int panelWidth;
+	private Ellipse[] planetCors;;
+	//private Queries searcher;
 	
+	
+	public static void main(String[] args) {
+		SolarSystem frame = new SolarSystem();
+		frame.showBackground();
+
+	}
 	
 	SolarSystem(){
 		display = new DisplayImage();
+		display.setVisible(true);
+		display.addMouseListener(this);
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBackground(Color.white);
 		setPreferredSize(new Dimension(500,250));
-		//setPreferredSize(display.getSize());
 		setLayout(new BorderLayout());
 		setUpFrame();
 		setVisible(true);
@@ -36,7 +56,8 @@ public class SolarSystem extends JFrame implements ActionListener {
 	}
 	
 	private void setUpFrame() {
-		int panelWidth = 80;
+		buttons = new JButton[5];
+		panelWidth = 80;
 		int spacing = 10;
 		Container c = getContentPane();
 		Color panelColor = new Color(100,145,202);
@@ -48,6 +69,7 @@ public class SolarSystem extends JFrame implements ActionListener {
 			pane.setBackground(Color.black);
 			pane.setLayout(new BorderLayout());
 			pane.add(display, BorderLayout.CENTER);
+			display.addBuffer(panelWidth);
 			c.add(pane, BorderLayout.CENTER);
 		
 		
@@ -56,7 +78,7 @@ public class SolarSystem extends JFrame implements ActionListener {
 			pane.setBackground(panelColor);
 			pane.setPreferredSize(new Dimension(panelWidth,0));
 			pane.setLayout(new BoxLayout(pane,BoxLayout.Y_AXIS));
-			pane.add(Box.createVerticalStrut(15));
+			pane.add(Box.createVerticalStrut(25));
 			pane.add(Box.createVerticalGlue());
 			
 			
@@ -69,7 +91,7 @@ public class SolarSystem extends JFrame implements ActionListener {
 				pane.add(Box.createVerticalStrut(spacing));
 			
 
-			button = new JButton("Data");
+			button = new JButton("Menu");
 				button.setLayout(new BoxLayout(button, BoxLayout.Y_AXIS));
 				button.setBackground(buttonColor);
 				button.addActionListener(this);
@@ -84,7 +106,7 @@ public class SolarSystem extends JFrame implements ActionListener {
 				pane.add(Box.createVerticalStrut(spacing));
 				
 			
-			button = new JButton("2");
+			button = new JButton("Save");
 				button.addActionListener(this);
 				buttons[2] = button;
 				button.setEnabled(false);
@@ -92,7 +114,7 @@ public class SolarSystem extends JFrame implements ActionListener {
 				pane.add(Box.createVerticalStrut(spacing));
 				
 			
-			button = new JButton("3");
+			button = new JButton("Next");
 				button.addActionListener(this);
 				buttons[3] = button;
 				button.setEnabled(false);
@@ -100,7 +122,7 @@ public class SolarSystem extends JFrame implements ActionListener {
 				pane.add(Box.createVerticalStrut(spacing));
 				
 			
-			button = new JButton("4");
+			button = new JButton("Back");
 				button.addActionListener(this);
 				buttons[4] = button;
 				button.setEnabled(false);
@@ -122,25 +144,96 @@ public class SolarSystem extends JFrame implements ActionListener {
 		switch(command) {
 		
 		case "Help":
+			showHelp();
+			createClickableRegions();
 			break;
 		
-		case "":
+		case "Menu":
+			showBackground();
 			break;
 		
+		case "Save":
+			break;
+			
 		default:
 			break;
 		}
 		
 	}
 	
-	public static void main(String[] args) {
-		SolarSystem frame = new SolarSystem();
-		frame.showBackground();
-
+	private void showHelp() {		
+        JOptionPane.showMessageDialog(this, "Click a planet to see a NASA image of it!", null, -1);
 	}
 
+	
 	private void showBackground() {
-		display.paint(display.getGraphics(), 0);
+		display.drawBackground(display.getGraphics());
+		buttonsOn(false);
+		createClickableRegions();
+		
+		setPreferredSize(display.getSize());
+		setResizable(false);
+		pack();
+		
+	}
+	
+	
+	private void enableClickableRegions(boolean b) {
+		
+	}
+
+	private void buttonsOn(boolean b) {
+		for(int k = 1; k < buttons.length; k++) {
+			buttons[k].setEnabled(b);
+		}
+	}
+	
+	private void createClickableRegions() {
+		planetCors = new Ellipse[8];
+		planetCors[0] = new Ellipse(16, 212, 38, 37, Color.black,"Mercury");
+		planetCors[1] = new Ellipse(68, 199, 71, 71, Color.black, "Venus");
+		planetCors[2] = new Ellipse(153, 192, 85, 85, Color.black, "Earth");
+		planetCors[3] = new Ellipse(252, 205, 60, 60, Color.black, "Mars");
+		planetCors[4] = new Ellipse(326, 159, 151, 151, Color.black, "Jupiter");
+		planetCors[5] = new Ellipse(522, 174, 112, 113, Color.black, "Saturn");
+		planetCors[6] = new Ellipse(685, 173, 124, 124, Color.black, "Uranus");
+		planetCors[7] = new Ellipse(823, 171, 127, 127, Color.black, "Neptune");
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent me) {
+		if(!buttons[1].isEnabled()) {
+			for(Ellipse e:planetCors) {
+				if(e.contains(me.getPoint())) {
+					//searcher.searchPlanet(e.getPlanetName());
+					buttonsOn(true);
+				}
+			}
+		}
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
 }
